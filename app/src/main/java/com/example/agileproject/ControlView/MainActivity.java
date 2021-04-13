b8d8f29d85126237292ee0dfd058feb038394bd1
William Hugo <william.hugo@hotmail.se>
2021-04-13 13:18:10 +0200

    Connected activity_main.xml to MainActivity.java
    Switching pages now works

--- app/src/main/java/com/example/agileproject/ControlView/MainActivity.java
+++ app/src/main/java/com/example/agileproject/ControlView/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.agileproject.ControlView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
 import com.example.agileproject.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +17,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.nav_bar);
+        NavController navController = Navigation.findNavController(this, R.id.fragment);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
     }
 }
