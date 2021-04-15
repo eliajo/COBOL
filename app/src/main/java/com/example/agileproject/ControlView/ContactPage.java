c04d4c823ecfe10e4c31a67d7a29f4024eb44678
elinberthag <74421256+elinberthag@users.noreply.github.com>
2021-04-15 13:19:04 +0200

    New XML contact page
    New XML contact page
    
    Co-Authored-By: klarajak <81352364+klarajak@users.noreply.github.com>

--- app/src/main/java/com/example/agileproject/ControlView/ContactPage.java
+++ app/src/main/java/com/example/agileproject/ControlView/ContactPage.java
@@ -60,7 +60,13 @@ public class ContactPage extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contact_page, container, false);
+        View v = inflater.inflate(R.layout.fragment_contact_page, container, false);
+
+        
+
+        return v;
     }
 }
