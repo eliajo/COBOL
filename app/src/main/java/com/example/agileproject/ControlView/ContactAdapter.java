22c6806fe1ff90367ed4857c12e09d0c28364be2
klarajak <81352364+klarajak@users.noreply.github.com>
2021-04-16 13:22:01 +0200

    Update ContactAdapter.java

--- app/src/main/java/com/example/agileproject/ControlView/ContactAdapter.java
+++ app/src/main/java/com/example/agileproject/ControlView/ContactAdapter.java
@@ -2,7 +2,6 @@ package com.example.agileproject.ControlView;
 
 import android.view.View;
 import android.view.ViewGroup;
-
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.RecyclerView.Adapter;
