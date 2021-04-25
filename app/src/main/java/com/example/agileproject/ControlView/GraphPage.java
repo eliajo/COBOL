603566e943810b73e6fed205c10a59ec00bab503
eliajo <eliascarlsward@gmail.com>
2021-04-26 00:15:50 +0200

    Continued with graph demo. Created a class to make Entries work with questionId. Made graphadapter more general and should work for different graphs with little effort

--- app/src/main/java/com/example/agileproject/ControlView/GraphPage.java
+++ app/src/main/java/com/example/agileproject/ControlView/GraphPage.java
@@ -13,16 +13,13 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.agileproject.Model.Answerable;
+import com.example.agileproject.Model.AnswerEntry;
 import com.example.agileproject.Model.GraphHelper;
 import com.example.agileproject.Model.NumberAnswer;
 import com.example.agileproject.R;
 import com.example.agileproject.Utils.FileConverter;
 import com.example.agileproject.Utils.FileFormatter;
-import com.github.mikephil.charting.charts.LineChart;
-import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.data.LineData;
-import com.github.mikephil.charting.data.LineDataSet;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -55,16 +52,32 @@ public class GraphPage extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_graph_page, container, false);
-     /*   LineChart chart = v.findViewById(R.id.chart);
+
         List<Answerable> answerableList = new ArrayList<>();
+
+
+        //All this is just demo functionality for now
         NumberAnswer na1 = new NumberAnswer(5,10,"2021-01-01");
         NumberAnswer na2 = new NumberAnswer(6,10,"2021-01-02");
         NumberAnswer na3 = new NumberAnswer(7,10,"2021-01-03");
+        NumberAnswer na4 = new NumberAnswer(5,10,"2021-01-04");
+        NumberAnswer na5 = new NumberAnswer(6,10,"2021-01-05");
+        NumberAnswer na6 = new NumberAnswer(7,10,"2021-01-06");
+
+        NumberAnswer s1 = new NumberAnswer(3,20,"2021-01-01");
+        NumberAnswer s2 = new NumberAnswer(3,20,"2021-01-02");
+        NumberAnswer s3 = new NumberAnswer(3,20,"2021-01-03");
 
 
         answerableList.add(na1);
         answerableList.add(na2);
         answerableList.add(na3);
+        answerableList.add(na4);
+        answerableList.add(na5);
+        answerableList.add(na6);
+        answerableList.add(s1);
+        answerableList.add(s2);
+        answerableList.add(s3);
         FileFormatter ff = new FileFormatter();
         String save =ff.format(answerableList);
 
@@ -72,40 +85,40 @@ public class GraphPage extends Fragment {
         fileConverter.convert(save);
 
         GraphHelper graphHelper = new GraphHelper();
-        answerableList = graphHelper.getDataFromDateToDate("2021-01-01","2021-01-03",10);
+        List<Answerable> wellBeingList = new ArrayList<>();
+        List<Answerable> sleepList = new ArrayList<>();
+        wellBeingList = graphHelper.getDataFromDateToDate("2021-01-01","2021-01-06",10);
+        sleepList = graphHelper.getDataFromDateToDate("2021-01-01","2021-01-03",20);
         int index = 0;
-        List<Entry> entries = new ArrayList<>();
 
-        for (Answerable a:answerableList) {
+        List<List<AnswerEntry>> entries = new ArrayList<>();
+
+        List<AnswerEntry> wellBeingEntry = new ArrayList<>();
+        List<AnswerEntry> sleepEntry = new ArrayList<>();
+
+        for (Answerable a:wellBeingList) {
             int tmp = (Integer) a.getData();
-            entries.add(new Entry(index,1.0f*tmp));
+            wellBeingEntry.add(new AnswerEntry(index,1.0f*tmp,a.getQuestionId()));
+            index++;
+        }
+        index=0;
+
+        for (Answerable a:sleepList) {
+            int tmp = (Integer) a.getData();
+            sleepEntry.add(new AnswerEntry(index,1.0f*tmp,a.getQuestionId()));
             index++;
         }
 
-        LineDataSet lineDataSet = new LineDataSet(entries, "Test");
+        entries.add(wellBeingEntry);
+        entries.add(sleepEntry);
 
-        LineData lineData = new LineData(lineDataSet);
-        chart.getAxisRight().setEnabled(false);
-       chart.getXAxis().setAxisMaximum(entries.size());
-        chart.getXAxis().setAxisMinimum(0f);
-        chart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
-        //chart.setVisibleXRange(entries.size()-1,entries.size()-1);
-        chart.getAxisLeft().setAxisMaximum(10);
-        chart.getAxisLeft().setAxisMinimum(0);
-
-        //chart.setVisibleXRangeMinimum(0);
-        //chart.setVisibleXRangeMaximum(10);
-        chart.setData(lineData);
-        chart.fitScreen();
-        chart.invalidate();
-*/
 
         RecyclerView recyclerView = v.findViewById(R.id.graph_recycler);
 
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this.getContext());
         recyclerView.setLayoutManager(layoutManager);
 
-        GraphAdapter graphAdapter = new GraphAdapter(getContext());
+        GraphAdapter graphAdapter = new GraphAdapter(getContext(),entries);
 
         recyclerView.setAdapter(graphAdapter);
 
