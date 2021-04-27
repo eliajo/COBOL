18fdf081b2fdc966c0146120a8052e4be84f92b0
William Hugo <william.hugo@hotmail.se>
2021-04-27 18:40:08 +0200

    Functionality for adding all events added, testing still needed

--- app/src/main/java/com/example/agileproject/ControlView/CalendarPage.java
+++ app/src/main/java/com/example/agileproject/ControlView/CalendarPage.java
@@ -1,7 +1,6 @@
 package com.example.agileproject.ControlView;
 
 import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -12,11 +11,13 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.agileproject.R;
+import com.example.agileproject.Utils.FileConverter;
 import com.roomorama.caldroid.CaldroidFragment;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.HashMap;
+import java.util.List;
 
 
 /**
@@ -39,21 +40,22 @@ public class CalendarPage extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        CaldroidFragment caldroidFragment = new CaldroidFragment();
+        CaldroidFragment caldroid = new CaldroidFragment();
         Bundle args = new Bundle();
-        Calendar cal = Calendar.getInstance();
-        args.putInt(CaldroidFragment.MONTH, cal.get(Calendar.MONTH) + 1);
-        args.putInt(CaldroidFragment.YEAR, cal.get(Calendar.YEAR));
+        Calendar calendar = Calendar.getInstance();
+        args.putInt(CaldroidFragment.MONTH, calendar.get(Calendar.MONTH) + 1);
+        args.putInt(CaldroidFragment.YEAR, calendar.get(Calendar.YEAR));
         args.putInt(CaldroidFragment.START_DAY_OF_WEEK, CaldroidFragment.MONDAY);
-        caldroidFragment.setArguments(args);
+        caldroid.setArguments(args);
 
         //ColorDrawable green = new ColorDrawable(getResources().getColor(R.color.green));
-        //Date date = new Date(2021-1900,3,25);
+        //Date date = new Date(2021-1900,4-1,25);
         //caldroidFragment.setBackgroundDrawableForDate(green, date);
         //caldroidFragment.refreshView();
+        addAllEvents(caldroid);
 
         FragmentTransaction t = getFragmentManager().beginTransaction();
-        t.replace(R.id.calendarView, caldroidFragment);
+        t.replace(R.id.calendarView, caldroid);
         t.commit();
     }
 
@@ -62,4 +64,33 @@ public class CalendarPage extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_calendar_page, container, false);
     }
+
+    private void addAllEvents(CaldroidFragment cal) {
+        List<Date> dates = fetchDatesAndFormat();
+        for(Date d : dates) {
+            addEventColor(cal, d);
+        }
+    }
+
+    /**
+     * Fetches all the dates of answered quizzes from FileConverter and formats the dates into the right format for the calendar
+     */
+    private List<Date> fetchDatesAndFormat() {
+        List<String> stringDates = FileConverter.getInstance().getAllDates();
+        List<Date> dates = new ArrayList<>();
+        for(String s : stringDates) {
+            String[] array = s.split("-"); //splits the dateString into an array of three strings, [0] = year, [1] = month, [2] = day
+            dates.add(new Date(Integer.parseInt(array[0])-1900,Integer.parseInt(array[1])-1,Integer.parseInt(array[2])));
+        }
+        return dates;
+    }
+
+    /**
+     * Adds a color to events, used for marking which days quizzes were done
+     */
+    private void addEventColor(CaldroidFragment cal, Date date) {
+        ColorDrawable green = new ColorDrawable(getResources().getColor(R.color.green));
+        cal.setBackgroundDrawableForDate(green, date);
+    }
+
 }
