fbcea2df8c256cc4c1c7e1212876175c5aee9e04
Edenia97 <57448561+Edenia97@users.noreply.github.com>
2021-04-27 10:26:00 +0200

    Final changes

--- app/src/main/java/com/example/agileproject/ControlView/ComplementaryQuestion3.java
+++ app/src/main/java/com/example/agileproject/ControlView/ComplementaryQuestion3.java
@@ -11,56 +11,17 @@ import androidx.navigation.Navigation;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.example.agileproject.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link ComplementaryQuestion3#newInstance} factory method to
- * create an instance of this fragment.
  */
 public class ComplementaryQuestion3 extends Fragment {
     NavController navController;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ComplementaryQuestion3() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ComplementaryQuestion3.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ComplementaryQuestion3 newInstance(String param1, String param2) {
-        ComplementaryQuestion3 fragment = new ComplementaryQuestion3();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -74,21 +35,20 @@ public class ComplementaryQuestion3 extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         navController = Navigation.findNavController(view);
-
-        view.findViewById(R.id.back_toComplementary_q2).setOnClickListener(new View.OnClickListener() {
+        EditText editText=(EditText) view.findViewById(R.id.textInputSideEffects);
+        view.findViewById(R.id.next_button_ComplementaryQuestions3).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                navController.navigate(R.id.action_complementaryQuestion3_to_questions2);
+                navController.navigate(R.id.action_complementaryQuestion3_to_question4);
             }
         });
 
-        view.findViewById(R.id.back_toComplementary_q2).setOnClickListener(new View.OnClickListener() {
+        view.findViewById(R.id.otherSideEffectsChip).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                navController.navigate(R.id.action_complementaryQuestion3_to_complementaryQuestion24);
+                 editText.setVisibility(View.VISIBLE);
             }
         });
-
-
     }
+
 }
