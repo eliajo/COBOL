b423cbc5f83b16b6523cc81f7e5e45d19adf8742
klarajak <81352364+klarajak@users.noreply.github.com>
2021-04-27 09:44:18 +0200

    Update CalendarPage.java

--- app/src/main/java/com/example/agileproject/ControlView/CalendarPage.java
+++ app/src/main/java/com/example/agileproject/ControlView/CalendarPage.java
@@ -10,6 +10,8 @@ import android.view.ViewGroup;
 
 import com.example.agileproject.R;
 
+import java.util.Calendar;
+
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link CalendarPage#newInstance} factory method to
@@ -50,16 +52,13 @@ public class CalendarPage extends Fragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_calendar_page, container, false);
     }
