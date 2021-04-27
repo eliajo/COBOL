5868461685bfb6ee6d75e6949f2e30dd73f5b070
eliajo <eliascarlsward@gmail.com>
2021-04-27 10:51:34 +0200

    Created graphdrawer class and moved functionality there

--- app/src/main/java/com/example/agileproject/ControlView/GraphAdapter.java
+++ app/src/main/java/com/example/agileproject/ControlView/GraphAdapter.java
@@ -32,9 +32,11 @@ public class GraphAdapter extends RecyclerView.Adapter<GraphAdapter.GraphHolder>
 
     private Context context;
     private List<List<AnswerEntry>> entries;
+    private GraphDrawer graphDrawer;
     public GraphAdapter(Context context,List<List<AnswerEntry>> entries){
         this.context = context;
         this.entries = entries;
+        this.graphDrawer = new GraphDrawer();
     }
 
     @NonNull
@@ -48,47 +50,9 @@ public class GraphAdapter extends RecyclerView.Adapter<GraphAdapter.GraphHolder>
     @Override
     public void onBindViewHolder(@NonNull GraphHolder holder, int position) {
       //if(viewtype == linechartid (For later))
+        graphDrawer.drawLineChart(entries,holder,position);
 
-        LineChart chart = (LineChart) holder.getGraph();
 
-
-        if ((entries.get(position).get(0).getQuestionId()==10)){
-
-            holder.getMainLabel().setText("Hur du har rankat ditt välmående");
-        }
-
-        //Not nice, if someone knows a better way feel free to fix it.
-        //Think it should work though. This is because lineDataSet needs an Entry but
-        //we still need questionId from AnswerEntry
-        //List<? extends Entry> converterList = entries.get(position);
-
-        //Safer solution but but takes O(n) time. Might have to discuss this.
-        List<Entry> converterList = new ArrayList<>(entries.get(position));
-        LineDataSet lineDataSet = new LineDataSet(converterList, "Dagar");
-
-        //Temporary code to show graph
-
-        LineData lineData = new LineData(lineDataSet);
-        chart.getAxisRight().setEnabled(false);
-        chart.getXAxis().setAxisMaximum(entries.get(position).size());
-        chart.getXAxis().setAxisMinimum(0f);
-        chart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
-        //chart.setVisibleXRange(entries.size()-1,entries.size()-1);
-        chart.getAxisLeft().setAxisMaximum(10);
-        chart.getAxisLeft().setAxisMinimum(0);
-
-        chart.getAxisLeft().setGranularity(1f);
-
-        chart.getAxisLeft().setDrawGridLines(false);
-
-        chart.getXAxis().setGranularity(1f);
-        chart.getXAxis().setDrawGridLines(false);
-
-        //chart.setVisibleXRangeMinimum(0);
-        //chart.setVisibleXRangeMaximum(10);
-        chart.setData(lineData);
-        chart.fitScreen();
-        chart.invalidate();
     }
 
 
