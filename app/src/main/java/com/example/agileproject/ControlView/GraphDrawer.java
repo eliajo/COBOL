61d24f598232422a2064bd6b5ceb1d923e8c0d7e
alvaleufstedt <alva-l@hotmail.com>
2021-04-28 11:44:44 +0200

    Created pieGraphDrawer
    Co-Authored-By: elinberthag <74421256+elinberthag@users.noreply.github.com>

--- app/src/main/java/com/example/agileproject/ControlView/GraphDrawer.java
+++ app/src/main/java/com/example/agileproject/ControlView/GraphDrawer.java
@@ -1,13 +1,22 @@
 package com.example.agileproject.ControlView;
 
+import android.graphics.Color;
+
 import com.example.agileproject.Model.AnswerEntry;
+import com.example.agileproject.R;
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 public class GraphDrawer {
@@ -20,7 +29,6 @@ public class GraphDrawer {
 
             holder.getMainLabel().setText("Hur du har rankat ditt välmående");
         }
-
         //Not nice, if someone knows a better way feel free to fix it.
         //Think it should work though. This is because lineDataSet needs an Entry but
         //we still need questionId from AnswerEntry
@@ -55,4 +63,27 @@ public class GraphDrawer {
         chart.invalidate();
 
     }
+
+    public void drawPieChart(List<List<AnswerEntry>> entries, GraphAdapter.GraphHolder holder, int position) {
+        PieChart pieChart = (PieChart) holder.getGraph();
+        PieData pieData;
+        List<PieEntry> pieEntryList = new ArrayList<>(entries.get(position));
+
+            pieChart = pieChart.findViewById(R.id.piechart);
+            pieChart.setUsePercentValues(true);
+            PieDataSet pieDataSet = new PieDataSet(pieEntryList,"Ja");
+            pieDataSet.setLabel("");
+            pieDataSet.setColors(ColorTemplate.JOYFUL_COLORS);
+            pieData = new PieData(pieDataSet);
+            pieChart.setData(pieData);
+            pieChart.invalidate();
+            pieEntryList.add(new PieEntry(1,"Nej"));
+            pieEntryList.add(new PieEntry(2,"Ja"));
+            pieChart.setDrawHoleEnabled(false);
+
+    }
+
+
+
+
 }
