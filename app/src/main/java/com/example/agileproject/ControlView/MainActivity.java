7932be8dd39c43532d3142e8936bea3a8988e799
eliajo <eliascarlsward@gmail.com>
2021-04-30 20:04:40 +0200

    Added so contacts and answers are loaded in at app startup

--- app/src/main/java/com/example/agileproject/ControlView/MainActivity.java
+++ app/src/main/java/com/example/agileproject/ControlView/MainActivity.java
@@ -11,7 +11,12 @@ import android.view.View;
 import android.widget.Button;
 
 import com.example.agileproject.R;
+import com.example.agileproject.Utils.AnswerConverter;
+import com.example.agileproject.Utils.ContactConverter;
+import com.example.agileproject.Utils.FileHandler;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.io.File;
 
 /**
  * @author William Hugo
@@ -25,6 +30,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        FileHandler fileHandler = new FileHandler();
+        String answers = fileHandler.read(getApplicationContext(),"Answers.txt");
+        String contacts = fileHandler.read(getApplicationContext(),"Contacts.txt");
+
+        AnswerConverter.getInstance().convert(answers);
+        ContactConverter.getInstance().convert(contacts);
         //This code connects the navigation bar to the fragment for our four main pages
         BottomNavigationView bottomNavigationView = findViewById(R.id.nav_bar);
         NavController navController = Navigation.findNavController(this, R.id.main_pages_fragment);
