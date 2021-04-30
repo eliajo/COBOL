dcfb5d1353b90b59434c896ecec101e7dde052d0
Fatemeh9312 <pegahamanzadeh@gmail.com>
2021-04-30 08:19:48 +0200

    update

--- app/src/main/java/com/example/agileproject/ControlView/Fragment1_in_QuizActivity.java
+++ app/src/main/java/com/example/agileproject/ControlView/Fragment1_in_QuizActivity.java
@@ -1,20 +1,32 @@
 package com.example.agileproject.ControlView;
 
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
+import android.renderscript.Sampler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.example.agileproject.Model.Answerable;
+import com.example.agileproject.Model.NumberAnswer;
 import com.example.agileproject.R;
+import com.google.android.material.chip.Chip;
 import com.google.android.material.chip.ChipGroup;
+
+import java.sql.Time;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Pegah Amanzadeh
@@ -26,8 +38,10 @@ import com.google.android.material.chip.ChipGroup;
 public class Fragment1_in_QuizActivity extends Fragment {
 
     NavController navController;
+    Chip selectedChip;
+    List<Answerable> QuizAnswers = new ArrayList<>();
 
-
+    NumberAnswer question1;
 
 
     @Override
@@ -42,8 +56,10 @@ public class Fragment1_in_QuizActivity extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         navController = Navigation.findNavController(view);
+        ChipGroup chipGroup2 = (ChipGroup) view.findViewById(R.id.chipGroup2);
 
-            // switching to fragment 2
+
+        // switching to fragment 2
 
         view.findViewById(R.id.next_button_q2).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -51,8 +67,23 @@ public class Fragment1_in_QuizActivity extends Fragment {
                 navController.navigate(R.id.action_question1_to_questions2);
             }
         });
+        chipGroup2.setOnCheckedChangeListener(new ChipGroup.OnCheckedChangeListener() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
+            @Override
+            public void onCheckedChanged(ChipGroup chipGroup, int checkedId) {
+             selectedChip = view.findViewById(chipGroup.getCheckedChipId());
+               question1=new NumberAnswer(Integer.valueOf(selectedChip.getText().toString()),1, LocalDateTime.now().toString());
+
+               if(QuizAnswers.contains(question1)){ // vi kollar om svaret redan finns i listan,då tar bort
+                                                    // den gamla och lägg till nya svaret
+                  QuizAnswers.remove(question1);
+                  QuizAnswers.add(question1);
 
 
+               }
+
+            }
+        });
 
     }
 }
