aaf8cba08292819d0870cd56868c5fd391722652
Edenia97 <57448561+Edenia97@users.noreply.github.com>
2021-05-03 15:15:10 +0200

    Complementary question
    Changed functionality for complementary questions

--- app/src/main/java/com/example/agileproject/ControlView/Questions2.java
+++ app/src/main/java/com/example/agileproject/ControlView/Questions2.java
@@ -20,52 +20,18 @@ import com.example.agileproject.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link Questions2#newInstance} factory method to
- * create an instance of this fragment.
+
  */
 public class Questions2 extends Fragment {
 
     NavController navController;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public Questions2() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment BlankFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Questions2 newInstance(String param1, String param2) {
-        Questions2 fragment = new Questions2();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -84,18 +50,27 @@ public class Questions2 extends Fragment {
                  navController.navigate(R.id.action_questions2_to_questions3);
              }
          });
-         view.findViewById(R.id.chipJa).setOnClickListener(new View.OnClickListener() {
+         view.findViewById(R.id.chipNo).setOnClickListener(new View.OnClickListener() {
              @Override
              public void onClick(View v) {
-                 navController.navigate(R.id.action_questions2_to_complementaryQuestion24);
+                 view.findViewById(R.id.textView6).setVisibility(View.GONE);
+                 view.findViewById(R.id.chipGroup3).setVisibility(View.GONE);
+                 view.findViewById(R.id.textView10).setVisibility(View.VISIBLE);
+                 view.findViewById(R.id.chipGroup).setVisibility(View.VISIBLE);
+                 view.findViewById(R.id.imageView).setVisibility(View.VISIBLE);
+
+             }
+         });
+         view.findViewById(R.id.imageView).setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 view.findViewById(R.id.textView10).setVisibility(View.GONE);
+                 view.findViewById(R.id.chipGroup).setVisibility(View.GONE);
+                 view.findViewById(R.id.imageView).setVisibility(View.GONE);
+                 view.findViewById(R.id.textView6).setVisibility(View.VISIBLE);
+                 view.findViewById(R.id.chipGroup3).setVisibility(View.VISIBLE);
              }
          });
 
-       //view.findViewById(R.id.back_to_q1).setOnClickListener(new View.OnClickListener() {
-            //@Override
-            //public void onClick(View v) {
-                //navController.navigate(R.id.action_questions2_to_question1);
-            //}
-        //});
     }
 }
