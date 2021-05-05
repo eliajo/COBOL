f4b4ccc2a28ad891e45dede9e89e78916df470c7
eliajo <eliascarlsward@gmail.com>
2021-05-05 21:01:45 +0200

    Start of the notification system

--- app/src/main/java/com/example/agileproject/ControlView/MainActivity.java
+++ app/src/main/java/com/example/agileproject/ControlView/MainActivity.java
@@ -1,16 +1,19 @@
 package com.example.agileproject.ControlView;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import com.example.agileproject.R;
+import com.example.agileproject.Utils.AlarmHandler;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 /**
@@ -20,6 +23,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 public class MainActivity extends AppCompatActivity {
 
     private Button startQuiz;
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +33,10 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView bottomNavigationView = findViewById(R.id.nav_bar);
         NavController navController = Navigation.findNavController(this, R.id.main_pages_fragment);
         NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
+        AlarmHandler alarmHandler = new AlarmHandler();
+
+        alarmHandler.setAlarm(getApplicationContext(),21,AlarmHandler.REMINDER_TYPE);
 
     }
 
---
+++ app/src/main/java/com/example/agileproject/Utils/AlarmHandler.java
@@ -0,0 +1,31 @@
+package com.example.agileproject.Utils;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
+import java.util.Calendar;
+
+public class AlarmHandler {
+    public static final int REMINDER_TYPE = 0;
+    public static final int MEDICINE_TYPE = 1;
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public void setAlarm(Context context, int time, int type){
+    Intent intent = new Intent(context,AlarmReceiver.class);
+    //Might need to double check flags for different types of notifications. Right now only one Pending indent be created for one intend...
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context,type,intent,PendingIntent.FLAG_UPDATE_CURRENT|PendingIntent.FLAG_ONE_SHOT);
+
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(System.currentTimeMillis());
+        calendar.set(Calendar.HOUR_OF_DAY,time);
+
+        alarmManager.setAndAllowWhileIdle(AlarmManager.RTC_WAKEUP,calendar.getTimeInMillis(),pendingIntent);
+    }
+}
