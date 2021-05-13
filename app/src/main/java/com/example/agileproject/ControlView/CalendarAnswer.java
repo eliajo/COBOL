c4bce4ebdd160b501ba288264e445bd89698665c
klarajak <81352364+klarajak@users.noreply.github.com>
2021-05-13 17:04:08 +0200

    Calendar answer update
    Co-Authored-By: elinberthag <74421256+elinberthag@users.noreply.github.com>

--- app/src/main/java/com/example/agileproject/ControlView/CalendarAnswer.java
+++ app/src/main/java/com/example/agileproject/ControlView/CalendarAnswer.java
@@ -22,14 +22,7 @@ import java.util.List;
  */
 public class CalendarAnswer extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private String date;
 
     public CalendarAnswer() {
         // Required empty public constructor
@@ -39,27 +32,21 @@ public class CalendarAnswer extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
+
      * @return A new instance of fragment CalendarAnswer.
      */
     // TODO: Rename and change types and number of parameters
-    public static CalendarAnswer newInstance(String param1, String param2) {
+    public static CalendarAnswer newInstance() {
         CalendarAnswer fragment = new CalendarAnswer();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
 
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -70,7 +57,7 @@ public class CalendarAnswer extends Fragment {
     }
 
     // run when clicking on a date, returns the answer from a specific date on question 14
-    private String fetchAnswers(String date){
+    public String fetchAnswers(String date){
         List<Answerable> answersDate = AnswerConverter.getInstance().getAnswersByDate(date);
         for (Answerable a : answersDate){
             if (a.getQuestionId() == 14){
