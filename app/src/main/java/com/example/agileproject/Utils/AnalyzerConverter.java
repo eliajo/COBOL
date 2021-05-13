793753fdc0f6e2312d00efa570527a6415db1f5a
williamhugoGH <william.hugo@hotmail.se>
2021-05-13 13:02:34 +0200

    AnalyzerConverter converts string into list

--- app/src/main/java/com/example/agileproject/Utils/AnalyzerConverter.java
+++ app/src/main/java/com/example/agileproject/Utils/AnalyzerConverter.java
@@ -1,6 +1,8 @@
 package com.example.agileproject.Utils;
 
 import com.example.agileproject.Model.AnalyzerSettable;
+import com.example.agileproject.Model.AnalyzerSettingBoolean;
+import com.example.agileproject.Model.AnalyzerSettingNumber;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,4 +28,28 @@ public class AnalyzerConverter {
         }
         return answerAnalyzerConverter;
     }
+
+    public void convert(String data) {
+        analyzerList.clear();
+        if(data.length() == 0) {
+            return;
+        }
+
+        String[] analyzers = data.split("@@@---@@@---@@@"); //Splitting up different analyzers for different questions
+
+        for(String s : analyzers) {
+            String[] settings = s.split("###---###---###"); //Splitting up the different settings of one analyzer
+            switch (Integer.parseInt(settings[1])) { //This finds the type
+                case 1: //Numbers
+                    AnalyzerSettingNumber intSetting = new AnalyzerSettingNumber(Integer.parseInt(settings[0]), Integer.parseInt(settings[1]), Integer.parseInt(settings[2]));
+                    analyzerList.add(intSetting);
+                    break;
+                case 2: //Booleans
+                    AnalyzerSettingBoolean booleanSetting = new AnalyzerSettingBoolean(Integer.parseInt(settings[0]), Boolean.parseBoolean(settings[1]));
+                    analyzerList.add(booleanSetting);
+                    break;
+            }
+        }
+    }
+    
 }
