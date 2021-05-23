4dad469cfbc8ef98865f0d720dda979df595b703
eliajo <eliascarlsward@gmail.com>
2021-05-23 15:33:52 +0200

    Some fixes to backbutton in quiz

--- app/src/main/java/com/example/agileproject/ControlView/QuizActivity.java
+++ app/src/main/java/com/example/agileproject/ControlView/QuizActivity.java
@@ -28,14 +28,16 @@ public class QuizActivity extends AppCompatActivity {
 
         Fragment navHostFragment = getSupportFragmentManager().getPrimaryNavigationFragment();
         Fragment fragment = navHostFragment.getChildFragmentManager().getFragments().get(0);
-        if(fragment.getClass().equals(Questions3.class) && findViewById(R.id.chipYesSideEffects).isSelected()){
+        if(fragment.getClass().equals(Questions3.class) && findViewById(R.id.chipYesSideEffects).isSelected()&&findViewById(R.id.textView11).getVisibility()==View.VISIBLE){
+            //findViewById(R.id.chipYesSideEffects).setSelected(false);
             findViewById(R.id.textView11).setVisibility(View.GONE);
             findViewById(R.id.Side_Effects).setVisibility(View.GONE);
             findViewById(R.id.imageView3).setVisibility(View.GONE);
             findViewById(R.id.textInputSideEffects).setVisibility(View.GONE);
             findViewById(R.id.textView2).setVisibility(View.VISIBLE);
             findViewById(R.id.chipGroup).setVisibility(View.VISIBLE);
-        } else if(fragment.getClass().equals(Questions2.class) && findViewById(R.id.chipNo).isSelected()){
+        } else if(fragment.getClass().equals(Questions2.class) && findViewById(R.id.chipNo).isSelected()&&findViewById(R.id.textView10).getVisibility()==View.VISIBLE){
+           // findViewById(R.id.chipNo).setSelected(false);
             findViewById(R.id.textView10).setVisibility(View.GONE);
             findViewById(R.id.sideeffectsRadiobutton).setVisibility(View.GONE);
             findViewById(R.id.imageView).setVisibility(View.GONE);
@@ -85,10 +87,18 @@ public class QuizActivity extends AppCompatActivity {
     public static boolean isComplementaryQuestionForPageAnswered(int pageNumber){
             //Checks if there is an object in list with question id equal to 7 or 10
             if (pageNumber==2){
-                return QuizAnswers.stream().anyMatch(o -> o.getQuestionId()==71);
+                for (Answerable a:QuizAnswers) {
+                    if (a.getQuestionId()==7){
+                        if (!(Boolean) a.getData()){
+                            return true;
+                        }
+
+                    }
+
+                }
             }
             else if (pageNumber==3){
-                return QuizAnswers.stream().anyMatch(o -> o.getQuestionId()==10);
+                return QuizAnswers.stream().anyMatch(o -> o.getQuestionId()==10 && (Boolean) o.getData()==true);
             }
             return false;
         }
